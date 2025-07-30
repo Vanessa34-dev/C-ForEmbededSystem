38d3aab2239e139df51fc8a466d60923761423cd
Vanessa34-dev <aimbaovao34@gmail.com>
2025-07-30 21:02:43 +0000

    first commit

---
+++ fixed_types/build/CMakeFiles/fixed_types.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fixed_types.dir/fixed_types.cpp.o"
+  "fixed_types.pdb"
+  "fixed_types"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fixed_types.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
