dc8caf3cfbad3999a9592a022d75ba4335f552e2
Vanessa34-dev <aimbaovao34@gmail.com>
2025-07-31 13:21:27 +0000

    move fixed_types and sizet folder to cross_arch

---
+++ cross_arch/sizet/build/CMakeFiles/sizet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sizet.dir/sizet.cpp.o"
+  "sizet.pdb"
+  "sizet"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sizet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
