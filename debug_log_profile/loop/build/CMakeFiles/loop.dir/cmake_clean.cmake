3fd6cf307df45138b01e076ba081ff5deb504344
Vanessa34-dev <aimbaovao34@gmail.com>
2025-08-04 08:04:25 +0300

    explore thread

---
+++ debug_log_profile/loop/build/CMakeFiles/loop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/loop.dir/loop.cpp.o"
+  "loop.pdb"
+  "loop"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/loop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ thread/threads.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<chrono>
+#include<thread>
+void worker(int index){
+	for(int i=0;i<10;i++){
+		std::cout<<"worker "<<index<<" begins" <<std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(50));
+		std::cout<<"worker " <<index <<"end" <<std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	}
+}
+int main(){
+	std::thread worker1(worker,1);
+	std::thread worker2(worker,2);
+	worker1.join();
+	worker2.join();
+	std::cout<<"Done"<<std::endl;
+}
